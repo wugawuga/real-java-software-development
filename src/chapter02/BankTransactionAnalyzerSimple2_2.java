9cb6881904b46fb936b34c55573a70e478a55537
JungWuk Yun <dnr3655@naver.com>
2022-09-17 23:19:31 +0900

    feat: 총 금액 반환 메소드 추가, 해당 달 내역 반환 메소드 추가

--- src/chapter02/BankTransactionAnalyzerSimple2_2.java
+++ src/chapter02/BankTransactionAnalyzerSimple2_2.java
@@ -25,4 +25,23 @@ public class BankTransactionAnalyzerSimple2_2 {
 		System.out.println("The total for all transactions is " + calculateTotalAmount(bankTransactions));
 		System.out.println("Transactions in January " + selectInMonth(bankTransactions, Month.JANUARY));
 	}
+
+	public static List<BankTransaction> selectInMonth(final List<BankTransaction> bankTransactions, final Month month) {
+		final List<BankTransaction> bankTransactionsInMonth = new ArrayList<>();
+
+		for (final BankTransaction bankTransaction : bankTransactions) {
+			if (bankTransaction.getDate().getMonth() == month) {
+				bankTransactionsInMonth.add(bankTransaction);
+			}
+		}
+		return bankTransactionsInMonth;
+	}
+
+	public static double calculateTotalAmount(final List<BankTransaction> bankTransactions) {
+		double total = 0d;
+		for (final BankTransaction bankTransaction : bankTransactions) {
+			total += bankTransaction.getAmount();
+		}
+		return total;
+	}
 }
