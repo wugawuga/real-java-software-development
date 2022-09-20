f79269b087e6d5174853fc1d2897de1d4fff51c0
wugawuga <dnr3655@naver.com>
2022-09-20 22:15:37 +0900

    feat: 입출금 내역 파싱하는 인터페이스 정의

---
+++ src/chapter02/BankStatementParser.java
@@ -0,0 +1,9 @@
+package chapter02;
+
+import java.util.List;
+
+public interface BankStatementParser {
+	BankTransaction parseFrom(String line);
+
+	List<BankTransaction> parseLinesFrom(List<String> lines);
+}
