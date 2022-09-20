f7cc2b196b042dca1170f269b47058ad3730ac75
wugawuga <dnr3655@naver.com>
2022-09-20 21:46:56 +0900

    feat: CRUD 응집

---
+++ src/chapter02/BankTransactionDAO.java
@@ -0,0 +1,26 @@
+package chapter02;
+
+import java.time.LocalDate;
+
+public class BankTransactionDAO {
+
+	public BankTransaction create(final LocalDate date, final double amount, final String description) {
+
+		throw new UnsupportedOperationException();
+	}
+
+	public BankTransaction read(final LocalDate date, final double amount, final String description) {
+
+		throw new UnsupportedOperationException();
+	}
+
+	public BankTransaction update(final LocalDate date, final double amount, final String description) {
+
+		throw new UnsupportedOperationException();
+	}
+
+	public BankTransaction delete(final LocalDate date, final double amount, final String description) {
+
+		throw new UnsupportedOperationException();
+	}
+}
