1e7829566a389948e65168da14f74b77633b381a
wugawuga <dnr3655@naver.com>
2022-09-22 09:08:11 +0900

    chore: 테스트 폴더, junit4 추가

---
+++ test/chapter02/BankStatementCSVParserTest.java
@@ -0,0 +1,16 @@
+package chapter02;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BankStatementCSVParserTest {
+
+    @Test
+    public void parseFrom() {
+    }
+
+    @Test
+    public void parseLinesFrom() {
+    }
+}
