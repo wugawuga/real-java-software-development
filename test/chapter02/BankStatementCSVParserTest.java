5769627a62aa4620d813c96527bcd67b3cb0efde
wugawuga <dnr3655@naver.com>
2022-09-22 09:19:33 +0900

    test: assertEquals 메소드로 테스트

--- test/chapter02/BankStatementCSVParserTest.java
+++ test/chapter02/BankStatementCSVParserTest.java
@@ -1,16 +1,33 @@
 package chapter02;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
 
 import static org.junit.Assert.*;
 
 public class BankStatementCSVParserTest {
 
-    @Test
-    public void parseFrom() {
-    }
+    private final BankStatementParser statementParser = new BankStatementCSVParser();
 
     @Test
-    public void parseLinesFrom() {
+    public void shouldParseOneCorrectLine() throws Exception {
+        final String line = "22-09-2022,-50,Tesco";
+
+        final BankTransaction result = statementParser.parseFrom(line);
+
+        final BankTransaction expected = new BankTransaction(
+                LocalDate.of(2022, Month.SEPTEMBER, 22),
+                -50,
+                "Tesco"
+        );
+
+        final double tolerance = 0.0d;
+
+        Assert.assertEquals(expected.getDate(), result.getDate());
+        Assert.assertEquals(expected.getAmount(), result.getAmount(), tolerance);
+        Assert.assertEquals(expected.getDescription(), result.getDescription());
     }
 }
