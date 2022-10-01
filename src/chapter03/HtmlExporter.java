df8a96018a8984689dc5444a3d95f45e1b93f678
wugawuga <dnr3655@naver.com>
2022-10-01 19:40:44 +0900

    chore: statics -> statistics 이름 변경

---
+++ src/chapter03/HtmlExporter.java
@@ -0,0 +1,2 @@
+package chapter03;public class HtmlExporter {
+}
--- src/chapter03/SummaryStatistics.java
+++ src/chapter03/SummaryStatistics.java
@@ -1,13 +1,13 @@
 package chapter03;
 
-public class SummaryStatics {
+public class SummaryStatistics {
 
 	private final double sum;
 	private final double max;
 	private final double min;
 	private final double average;
 
-	public SummaryStatics(double sum, double max, double min, double average) {
+	public SummaryStatistics(double sum, double max, double min, double average) {
 		this.sum = sum;
 		this.max = max;
 		this.min = min;
